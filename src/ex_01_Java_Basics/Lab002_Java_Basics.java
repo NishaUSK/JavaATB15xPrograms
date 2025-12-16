8386d8b393f5f19d03b47ec72dad0a07d3f90726
Nisha USK <nishaug18@gmail.com>
2025-12-16 23:26:49 +0530

    First Java Program.

--- src/ex_01_Java_Basics/Lab002_Java_Basics.java
+++ src/ex_01_Java_Basics/Lab002_Java_Basics.java
@@ -1,4 +1,11 @@
 package ex_01_Java_Basics;
 
 public class Lab002_Java_Basics {
+    public static void main(String[] args) {
+        System.out.println(2+2);
+        System.out.println(4-2);
+        System.out.println(10/2);
+        System.out.println(2*2);
+        System.out.println(2+2*4/5);
+            }
 }
