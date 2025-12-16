e9248ea1b0b60cabe73b73339ce2d7033945fdf5
Nisha USK <nishaug18@gmail.com>
2025-12-17 00:07:45 +0530

    Commit.

---
+++ src/ex_02_Java_Basics/Lab008_Keywords_Identifiers.java
@@ -0,0 +1,12 @@
+package ex_02_Java_Basics;
+
+public class Lab008_Keywords_Identifiers {
+    public static void main(String[] args) {
+        System.out.println("Keywords");
+/**       Below are Keywords present in this program
+ *
+ * package, public, class, static, void
+ *
+ *  */
+    }
+}
