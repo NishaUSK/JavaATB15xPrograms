397c9a1238fd6d7f9854bd6b20ddec208033e1ca
Nisha USK <nishaug18@gmail.com>
2025-12-17 15:48:20 +0530

    Commit.

--- src/ex_02_Java_Basics/Lab009_Math_Operations.java
+++ src/ex_02_Java_Basics/Lab009_Math_Operations.java
@@ -9,6 +9,8 @@ public class Lab009_Math_Operations {
 //      System.out.println(6-6); It'll not support if the value or O/P will be zero
 //      System.out.println(6/6);
         System.out.println(60-20);
+//      System.out.println(60/0); It shows an error because in java we can't divide by zero/0
+        System.out.println(0/60);
 
     }
 }
---
+++ src/ex_02_Java_Basics/Lab011_MultipleClass.java
@@ -0,0 +1,10 @@
+package ex_02_Java_Basics;
+
+public class Lab011_MultipleClass {
+    }
+    class N{
+        public static void main(String[] args) {
+            System.out.println("Hello");
+        }
+    }
+
