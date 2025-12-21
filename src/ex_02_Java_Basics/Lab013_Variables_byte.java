ef7b0256d89455f44e20190a704fd2497aa62971
Nisha USK <nishaug18@gmail.com>
2025-12-21 14:41:10 +0530

    Variables_byte, commit.

---
+++ src/ex_02_Java_Basics/Lab013_Variables_byte.java
@@ -0,0 +1,19 @@
+package ex_02_Java_Basics;
+
+public class Lab013_Variables_byte
+{
+    public static void main(String[] args) {
+
+        // data_type variable_name =  variable_value
+
+        // Variable - Local Variable
+        byte age_Nisha = 29; // byte value is min -128 to max 127
+        // byte -> data type - type of container
+        //  age_sunil ->  variable_name |  identifier
+        // = -> Assignment Operator ?  (after some time)
+        // 45 -> Variable Value |  Literal
+        age_Nisha = 28;
+        age_Nisha = 30;
+        System.out.print(age_Nisha);
+    }
+}
